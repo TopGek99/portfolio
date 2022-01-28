eca25e398166cb12e2f06b45692e9db5b4209606
TopGek99 <arowe890@gmail.com>
2022-01-28 16:51:53 +1030

    begun implementation of TopBar component

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function TopBar() {
+  return (
+    <div id="topbar">
+      <a className="link" href="mailto:arowe890@gmail.com">
+        arowe890@gmail.com
+      </a>
+    </div>
+  );
+}
+
+export default TopBar;
